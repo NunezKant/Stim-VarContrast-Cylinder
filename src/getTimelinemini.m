426d2722e70d45012978c4cd995d21d941229ad8
NunezKant <35149535+NunezKant@users.noreply.github.com>
2023-05-30 16:41:01 -0400

    Add files via upload

--- src/getTimelinemini.m
+++ src/getTimelinemini.m
@@ -4,7 +4,8 @@ global Timeline
 
 % check if there is a new microscope frame
 Timeline.daq.ipack = TL.daq.ipack + 1;
-Timeline.daq.pack(TL.daq.ipack).time = now;
+tnow = now;
+Timeline.daq.pack(TL.daq.ipack).time = tnow;
 Timeline.daq.pack(TL.daq.ipack).data = event.Data(:,1);
 
 data = event.Data(:,1);
@@ -15,18 +16,15 @@ if (data(end) > 2) && (min(data) < 2)
     Timeline.daq.last_mic = Timeline.daq.mic_time(Timeline.daq.nframes);
 end
 
-newN = size(event.Data,1);
-nch = size(event.Data,2);
+newN = size(data,1);
 newTotal = Timeline.numSamples + newN;
 if newTotal>size(timeline.data,1)
     Timeline.data(2e6 + NewTotal, 1) = 0;
 end
 
-tnow = now;
 nSamples = Timeline.numSamples;
-%Timeline.data(nSamples + [1:newN], 1:nch) = event.Data;
-Timeline.data(nSamples + [1:newN], 2) = event.Data(:,1);
-Timeline.data(nSamples + newN, 3) = tnow;
+Timeline.data(nSamples + [1:newN], 1) = data;
+Timeline.data(nSamples + newN, 2) = tnow;
 Timeline.numSamples = NewTotal;
 Timeline.npacks = Timeline.npacks + 1;
 Timeline.packet(Timeline.npacks).nSamples = nSamples;
